399d39d180becd271b20403585a20a7d74bf0f3e
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-03-25 22:05:34 +0300

    Validate password and submit form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
       <label>Email:</label>
       <input type="email" required v-model="email">
 
       <label>Password:</label>
       <input type="password" required v-model="password">
+      <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
       <label>Role:</label>
       <select v-model="role">
@@ -21,6 +22,10 @@
       <div class="terms">
           <input type="checkbox" v-model="terms" required>
           <label>Accept terms and conditions</label>
+      </div>
+
+      <div class="submit">
+          <button>Create an account</button>
       </div>
   </form>
 
@@ -39,7 +44,8 @@ export default {
             role: 'designer',
             terms: false,
             tempSkill: '',
-            skills: []
+            skills: [],
+            passwordError: ''
 
         }
     },
@@ -56,6 +62,20 @@ export default {
             this.skills = this.skills.filter((item) => {
                 return skill != item
             })
+
+        },
+        submitForm() {
+            // validate password
+            this.passwordError = this.password.length > 5 ? 
+            '': 'Password must be at least 6 chars long'
+            if(!this.passwordError) {
+                console.log('email:', this.email)
+                console.log('password:', this.password)
+                console.log('role:', this.role)
+                console.log('skills:', this.skills)
+                console.log('terms accepted:', this.terms)
+            }
+
 
         }
     }
@@ -109,5 +129,21 @@ input[type="checkbox"] {
     color: #777;
     cursor: pointer;
 }
-
+button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+.submit {
+    text-align: center;
+}
+.error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
+}
 </style>
