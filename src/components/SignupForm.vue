27d66645bc4aabab43a7d12fca91823409197193
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-03-25 21:07:50 +0300

    V-model data binding with form inputs.

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,12 +1,28 @@
 <template>
   <form>
       <label>Email:</label>
-      <input type="email" required>
+      <input type="email" required v-model="email">
+
+      <label>Password:</label>
+      <input type="password" required v-model="password">
+
+      <label>Role:</label>
+      <select v-model="role">
+          <option value="developer">Web developer</option>
+          <option value="designer">Web designer</option>
+      </select>
   </form>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            role: 'designer',
+        }
+    }
 
 }
 </script>
@@ -24,12 +40,12 @@ label {
     color: #aaa;
     display: inline-block;
     margin: 25px 0 15px;
-    font-size: 0.6em;
+    font-size: 1.0em;
     text-transform: uppercase;
     letter-spacing: 1px;
     font-weight: bold;
 }
-input {
+input, select {
     display: block;
     padding: 10px 6px;
     width: 100%;
