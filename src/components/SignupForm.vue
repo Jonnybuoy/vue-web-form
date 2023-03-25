e58390cd1af2943521cfae72ece0dd15616bd07a
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-03-25 21:45:08 +0300

    Click event

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -13,9 +13,9 @@
       </select>
 
       <label>Skills</label>
-      <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+      <input type="text" v-model="tempSkill" @keyup="addSkill">
       <div v-for="skill in skills" :key="skill" class="pill">
-          {{ skill }}
+          <span @click="deleteSkill(skill)">{{ skill }}</span>
       </div>
 
       <div class="terms">
@@ -49,9 +49,14 @@ export default {
                 if (!this.skills.includes(this.tempSkill)) {
                     this.skills.push(this.tempSkill)
                 }
-                
                 this.tempSkill = ''
             }
+        },
+        deleteSkill(skill) {
+            this.skills = this.skills.filter((item) => {
+                return skill != item
+            })
+
         }
     }
 
@@ -91,6 +96,18 @@ input[type="checkbox"] {
     margin: 0 10px 0 0;
     position: relative;
     top: 2px;
+}
+.pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
 }
 
 </style>
